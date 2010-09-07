5fa3fddc91657dc63a069caf040294e4020f4505
Scott Sibley <ssibley@gmail.com>
2010-09-07 19:45:21 +0000

    Bugfix

--- StarTip.lua
+++ StarTip.lua
@@ -1,5 +1,7 @@
 ﻿StarTip = LibStub("AceAddon-3.0"):NewAddon("StarTip: @project-version@", "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0", "AceTimer-3.0") 
 StarTip.version = GetAddOnMetadata("StarTip", "X-StarTip-Version") or ""
+MAJOR = StarTip.version
+
 local LibDBIcon = LibStub("LibDBIcon-1.0")
 local LSM = _G.LibStub("LibSharedMedia-3.0")
 local LDB = LibStub:GetLibrary("LibDataBroker-1.1")
