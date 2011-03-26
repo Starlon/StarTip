fb037012cb6a2158cd4ecb6ddfb40eafe7aa431c
Scott Sibley <ssibley@gmail.com>
2011-03-26 21:59:08 +0000

    Add localization files

---
+++ Localization/zhTW.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("StarTip", "zhTW", false, true)
+if not L then return end
+
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="concat", handle-unlocalized="english")@
